5085fbb57c3b0c9a9d1810b533df4d8e030ca71c
Sven Van Gastel <sven.v.gastel@gmail.com>
2022-08-09 14:18:36 +0200

    H6 enum

--- src/test/java/be/vdab/fietsen/repositories/JpaDocentRepositoryTest.java
+++ src/test/java/be/vdab/fietsen/repositories/JpaDocentRepositoryTest.java
@@ -1,13 +1,15 @@
 package be.vdab.fietsen.repositories;
 
+import be.vdab.fietsen.domain.Geslacht;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
-@DataJpaTest (showSql = false)
+@DataJpaTest(showSql = false)
 @Sql("/insertDocent.sql")
 @Import(JpaDocentRepository.class)
 class JpaDocentRepositoryTest extends AbstractTransactionalJUnit4SpringContextTests {
@@ -29,5 +31,19 @@ class JpaDocentRepositoryTest extends AbstractTransactionalJUnit4SpringContextTe
     @Test
     void findByOnbestaandeId() {
         assertThat(repository.findById(-1)).isEmpty();
+    }
+
+    private long idVanTestVrouw() {
+        return jdbcTemplate.queryForObject("select id from docenten where voornaam = 'testV'", Long.class);
+    }
+
+    @Test
+    void man() {
+        assertThat(repository.findById(idVanTestMan())).hasValueSatisfying(docent -> assertThat(docent.getGeslacht()).isEqualTo(Geslacht.MAN));
+    }
+
+    @Test
+    void vrouw() {
+        assertThat(repository.findById(idVanTestVrouw())).hasValueSatisfying(docent -> assertThat(docent.getGeslacht()).isEqualTo(Geslacht.VROUW));
     }
 }
